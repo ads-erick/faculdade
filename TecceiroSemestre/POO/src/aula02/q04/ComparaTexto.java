193ebf9e6fd623de4eaef308ecb09babae6e6fa7
Erick <erick.ads@outlook.com>
2026-03-16 23:56:04 -0300

    poo-q04

--- TecceiroSemestre/POO/src/aula02/q04/ComparaTexto.java
+++ TecceiroSemestre/POO/src/aula02/q04/ComparaTexto.java
@@ -1,4 +1,31 @@
+//Escreva um programa em java que receba dois textos. Verifique se os dois
+//textos são iguais e apresente o resultado em tela. Observe e analise os
+//resultados.
+
 package aula02.q04;
 
+import java.util.Scanner;
+
 public class ComparaTexto {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Comparação de textos");
+
+        System.out.println("Insira o primeiro texto:");
+        String text1 = scanner.nextLine();
+
+        System.out.println("Insira a segunda frase:");
+        String text2 = scanner.nextLine();
+
+        if (text1.equals(text2)) {
+            System.out.println("Os textos são iguais.");
+        } else {
+            System.out.println("Os textos não são iguais.");
+        }
+
+        scanner.close();
+
+    }
 }
