c8127a32fcf010a45116365736d30d3e6d2fa5d9
Erick <erick.ads@outlook.com>
2026-03-18 17:29:12 -0300

    poo-a03-q03

--- TecceiroSemestre/POO/src/aula03/q02/SomaInteiroFor.java
+++ TecceiroSemestre/POO/src/aula03/q02/SomaInteiroFor.java
@@ -2,25 +2,23 @@
 //nu´mero 1 at´e o valor informado pelo usu´ario. Apresente o resultado em
 //tela.
 
-package aula03.qo2;
+package aula03.q02;
 
 import java.util.Scanner;
 
-public class SomaInteiroWhile {
-    public static void main (String[] args) {
+public class SomaInteiroFor {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite um número inteiro: ");
         int num = scanner.nextInt();
 
-        int num2 = 1;
         int soma = 0;
 
-        while (num2<=num) {
-            soma += num2;
-            num2++;
+        for (int i = 1; i<= num; i++) {
+            soma += i;
         }
-        System.out.print(soma);
+        System.out.println(soma);
     }
 }
--- TecceiroSemestre/POO/src/aula03/q02/SomaInteiroWhile.java
+++ TecceiroSemestre/POO/src/aula03/q02/SomaInteiroWhile.java
@@ -2,23 +2,25 @@
 //nu´mero 1 at´e o valor informado pelo usu´ario. Apresente o resultado em
 //tela.
 
-package aula03.qo2;
+package aula03.q02;
 
 import java.util.Scanner;
 
-public class SomaInteiroFor {
-    public static void main(String[] args) {
+public class SomaInteiroWhile {
+    public static void main (String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite um número inteiro: ");
         int num = scanner.nextInt();
 
+        int num2 = 1;
         int soma = 0;
 
-        for (int i = 1; i<= num; i++) {
-            soma += i;
+        while (num2<=num) {
+            soma += num2;
+            num2++;
         }
-        System.out.println(soma);
+        System.out.print(soma);
     }
 }
