5bd6b81a7b3a122a1b5c7dd22498f03ed45d6237
Erick <erick.ads@outlook.com>
2026-03-17 23:17:46 -0300

    poo-q06

--- TecceiroSemestre/POO/src/aula02/q06/Teste.java
+++ TecceiroSemestre/POO/src/aula02/q06/Teste.java
@@ -1,4 +1,24 @@
+//Implemente e analise o trecho de código abaixo e identifique a diferença
+//entre os operadores.
+//int x = 10;
+//int y = 10;
+//3 System . out . println ( x ++) ;
+//4 System . out . println (++ y ) ;
+//5 System . out . println ( " x = " + x ) ;
+//6 System . out . println ( " y = " + y ) ;
+
 package aula02.q06;
 
 public class Teste {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        int y = 10;
+
+        System . out . println ( x ++) ;
+        System . out . println (++ y ) ;
+        System . out . println ( " x = " + x ) ;
+        System . out . println ( " y = " + y ) ;
+    }
 }
