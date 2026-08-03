44b83663270e731685bc8d0513aa64fa2f466484
Erick <erick.ads@outlook.com>
2026-08-03 19:06:17 -0300

    chore: alterar nome de pastas

---
+++ terceiro_semestre/POO/src/aula20260522/lista/Main.java
@@ -0,0 +1,27 @@
+package aula20260522.lista;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // sequencia
+        // conjunto de objetos que tem ordem e podem repetir
+        // 1, 2, 3, 4
+        // 4, 3, 2, 1
+        // 1, 1, 1, 1
+        // <Integer> -> tipo de dados que sera armazenado
+        // ArrayList -> uma lista que guarda os elementos em um arranjo
+
+        var lista2 = new ArrayList<>();
+
+        // List.of -> retorna uma lista imutavel
+        var lista3 = List.of(1, 2, 3, 4, 5);
+
+        // Double, Integer, Float, Boolean, Character são objetos, então podem ter valor nulo
+        Double d = null;
+        List<Double> numeros;
+
+    }
+
+}
